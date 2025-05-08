45991a3207f8d030d08ab031c56e1b635f8915f1
jaden <96341029+Verpxnter@users.noreply.github.com>
2025-05-08 21:15:27 +0200

    Added Table To See Files

--- src/main/java/de/jadenk/springcloud/controller/AuthController.java
+++ src/main/java/de/jadenk/springcloud/controller/AuthController.java
@@ -1,17 +1,24 @@
 package de.jadenk.springcloud.controller;
 
+import de.jadenk.springcloud.model.UploadedFile;
 import de.jadenk.springcloud.model.User;
+import de.jadenk.springcloud.repository.UploadedFileRepository;
 import de.jadenk.springcloud.service.FileUploadService;
 import de.jadenk.springcloud.service.LogService;
 import de.jadenk.springcloud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class AuthController {
@@ -39,12 +46,19 @@ public class AuthController {
         return "redirect:/login";
     }
 
+    @Autowired
+    private UploadedFileRepository uploadedFileRepository;
+
     @GetMapping("/dashboard")
     public String dashboard(Model model, Authentication authentication) {
         if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();  // Benutzername aus dem Authentication-Objekt
-            model.addAttribute("username", username);  // Benutzernamen zum Modell hinzufügen
+            String username = authentication.getName();
+            model.addAttribute("username", username);
         }
+
+        List<UploadedFile> files = uploadedFileRepository.findAll(); // alternativ: nur vom eingeloggten User
+        model.addAttribute("files", files);
+
         return "dashboard";
     }
 
@@ -64,6 +78,21 @@ public class AuthController {
         } else {
             return "redirect:/dashboard?fileEmpty";
         }
+    }
+
+    @GetMapping("/download/{id}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Long id) {
+        UploadedFile file = uploadedFileRepository.findById(id).orElseThrow();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFileName() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, file.getFileType())
+                .body(new ByteArrayResource(file.getFileData()));
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteFile(@PathVariable Long id) {
+        uploadedFileRepository.deleteById(id);
+        return "redirect:/dashboard";
     }
 
 }
