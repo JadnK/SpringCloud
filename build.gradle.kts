c2b9851dba4b69b3cf1496d521dc8f8daa71c275
jaden <96341029+Verpxnter@users.noreply.github.com>
2025-09-10 13:16:23 +0200

    Made Css with Tailwind/NPM for @apply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,40 @@
+import com.github.gradle.node.npm.task.NpmTask
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.github.node-gradle.node") version "7.1.0"
+}
+
+node {
+	version.set("22.19.0")
+	npmVersion.set("10.9.3")
+	download.set(true)
 }
 
 group = "de.jadenk"
 version = "0.0.1-SNAPSHOT"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
-
 repositories {
 	mavenCentral()
+	gradlePluginPortal()
+}
+
+tasks.register<NpmTask>("tailwindBuild") {
+	group = "build"
+	description = "Build Tailwind CSS in-place"
+	args.set(listOf("run", "build:css")) // equivalent to 'npm run build:css'
+}
+
+tasks.named("processResources") {
+	dependsOn("tailwindBuild")
+}
+
+java {
+	toolchain {
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
 }
 
 dependencies {
@@ -24,7 +44,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("org.json:json:20240303")
-
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mockito:mockito-core:5.12.0")
@@ -38,4 +57,3 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
