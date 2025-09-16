2235e0599843f48320a360b50c4d55a59be9bf74
jaden <96341029+Verpxnter@users.noreply.github.com>
2025-09-16 14:46:02 +0200

    Version Service Update to 3.2

--- src/main/java/de/jadenk/springcloud/service/VersionService.java
+++ src/main/java/de/jadenk/springcloud/service/VersionService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VersionService {
-    private final String currentVersion = "3.1";
+    private final String currentVersion = "3.2";
 
     public String getCurrentVersion() {
         return currentVersion;
